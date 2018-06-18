ceb0c39b67745075e1c983cf6e5df0e1c25298e5
Nicolas <m26416088@john.petra.ac.id>
2018-06-18 23:04:20 +0800

    jkaeylsk

--- homecafe.php
+++ homecafe.php
@@ -1,13 +1,31 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script type="js/bootstrap.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<html lang="en">
 
-<?php
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Seljeff</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+  </head>
+  <?php
   $con = mysqli_connect("localhost", "root", "", "dbcafe");
   if (isset($_POST['login']))
   {
@@ -50,126 +68,265 @@
   }
 ?>
 
-<style type="text/css">
-	.carousel-inner{
-		height: 700px;
-	}
-  #map{
-    height: 64%;
-    width: 64%;
-  }
-	.carousel-caption{
-		padding-bottom: 300px;
-	}
-	.container-fluid{
-		padding-top: 10px;
-		height: 80px;
-		font-size: 20px;
-	}
-</style>
-<nav class="navbar navbar-expand-lg navbar-light">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="homecafe.php" style="margin-right: 500px;">SelJeff</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li><a href="homecafe.php">Home</a></li>
-      <li><a href="#">Menu</a></li>
-      <li><a href="#">Event</a></li>
-     <ul class="nav navbar-nav navbar-right">
-     <li class="dropdown">
-     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-     <ul id="login-dp" class="dropdown-menu">
-     <li>
-     <div class="row">
-     <div class="col-md-12">
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">Seljeff</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#top">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Menu</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Event</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#login">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead text-center text-white d-flex" name="top">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase" style="background-color: #282a2d;">
+              <strong>Seljeff Restaurant</strong>
+            </h1>
+            <hr>
+          </div>
+          <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5" style="font-size:30px;font-weight: bold; color: #282a2d;">Bon Appetit</p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">About Us</a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">About</h2>
+            <hr class="light my-4">
+            <p class="text-faded mb-4">Seljeff Restaurant is the ultimate destination for your family. Dress up and try the Chef's latest experimental dish.</p>
+             <h2 class="section-heading text-white">-------------------------------------------------</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">At Your Service</h2>
+            <hr class="my-4">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fa fa-4x fa-group text-primary mb-3 sr-icons"></i>
+              <h3 class="mb-3">Best Quality Food</h3>
+              <p class="text-muted mb-0">Our foods come from the best Worldwide Chefs.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
+              <h3 class="mb-3">Fast Service</h3>
+              <p class="text-muted mb-0">Our services is fast and responsive.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <i class="fa fa-4x fa-heart text-primary mb-3 sr-icons"></i>
+              <h3 class="mb-3">Made with Love</h3>
+              <p class="text-muted mb-0">Our restaurant always welcoming.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="p-0" id="portfolio">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters popup-gallery">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Steak
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Rice
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Turkey
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/4.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Deserts
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/5.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                    Sushi
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/6.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Category
+                  </div>
+                  <div class="project-name">
+                   Pizza
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="bg-dark text-white">
+     <div class="container">
+        <div class="row">
+          <div class="col-lg-6 col-lg-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+             <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Menu</a>
+            </div>
+          </div>
+          <div class="col-lg-6 col-lg-6 text-center">
+            <div class="service-box mt-5 mx-auto">
+              <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Event</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="login">
+      <div class="container">
         <form action = homecafe.php method = "post">
-				Username:<br> <input type="text" name="username"><br><br>
-				Password:<br> <input type="password" name="password"><br><br>
-				<input type="submit" class="btn btn-success btn-block" name="login" value ="Login"><br>
-			</form>
-     </div>
-     <div class="bottom text-center">
-     New here ? <a href="signupcafe.php"><b>Join Us</b></a>
-     </div>
-     </div>
-     </li>
-     </ul>
-     </li>
-     </ul>
-     </ul>
- </nav>
-</head>
-<body>
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" >
-
-      <div class="item active">
-        <img src="1.jpg" alt="Los Angeles" style="width:100%;">
-        <div class="carousel-caption">
-          <h3>Food1Name</h3>
-          <p>food1desc</p>
+        Username:<br> <input type="text" class="form-control" name="username"><br><br>
+        Password:<br> <input type="password" class="form-control" name="password"><br><br>
+        <input type="submit" style="background-color:#f05f40; color:white;" class="btn btn-block" name="login" value ="Login"><br>
+        <div class="bottom text-center">
+        New here ? <a href="signupcafe.php"><b>Join Us</b></a>
         </div>
       </div>
+    </section>
 
-      <div class="item">
-        <img src="2.jpg" alt="Chicago" style="width:100%;">
-        <div class="carousel-caption">
-           <h3>Food2Name</h3>
-          <p>food2desc</p>
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Let's Get In Touch!</h2>
+            <hr class="my-4">
+            <p class="mb-5">Siwalankerto 146-148, Surabaya</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 ml-auto text-center">
+            <i class="fa fa-phone fa-3x mb-3 sr-contact"></i>
+            <p>123-456-6789</p>
+          </div>
+          <div class="col-lg-4 mr-auto text-center">
+            <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
+            <p>
+              <a href="mailto:your-email@your-domain.com">feedback@seljeff.com</a>
+            </p>
+          </div>
         </div>
       </div>
-    
-      <div class="item">
-        <img src="3.jpg" alt="New York" style="width:100%;">
-        <div class="carousel-caption">
-            <h3>Food3Name</h3>
-          <p>food3desc</p>
-        </div>
-      </div>
-  
-    </div>
+    </section>
 
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-  <div class="container">
-  	<br><br>
-  	 <div class="row">
-            <div class="col-md-4">
-            <img src="1.jpg" width="150" height="150" class="img-circle" style="margin-left: 70px;"><br><br>
-			       A steak (/ˈsteɪk/) is a meat generally sliced across the muscle fibers, potentially including a bone. Exceptions, in which the meat is sliced parallel to the fibers, include the skirt steak that is cut from the plate, the flank steak that is cut from the abdominal muscles, and the Silverfinger steak that is cut from the loin and includes three rib bones. When the word "steak" is used without qualification, it generally refers to a beefsteak.
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-            </div>
-            <div class="col-md-4">
-            <img src="2.jpg" width="150" height="150" class="img-circle" style="margin-left: 70px;"><br><br>
-            A fish steak, alternatively known as a fish cutlet, is a cut of fish which is cut perpendicular to the spine and can either include the bones or be boneless.[1] Fish steaks can be contrasted with fish fillets, which are cut parallel to either side of the spine and do not include the larger bones. In contrast to other vertebrate animals, over 85% of the fish body is made up of consumable muscle.
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
-            </div>
-            <div class="col-md-4">
-            <img src="3.jpg" width="150" height="150" class="img-circle" style="margin-left: 70px;" ><br><br>
-             A hamburger, beefburger or burger is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, barbecued, or flame broiled. Hamburgers are often served with cheese, lettuce, tomato, bacon, onion, pickles, or chiles; condiments such as mustard, mayonnaise, ketchup, relish, or "special sauce"; and are frequently placed on sesame seed buns
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
 
-            </div>                
-        </div>
-  </div>
-</body>
-<footer class="page-footer font-small mt-4">
+  </body>
 
-</footer>
 </html>
-
