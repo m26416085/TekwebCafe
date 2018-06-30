28b2a6a22462a43a22b6af10507fe56cdfc9f7e3
m26416085 <m26416085@john.petra.ac.id>
2018-06-30 18:03:41 +0700

    Minuman sudah ada query, eventcafe berisi event dan menu

--- eventcafe.php
+++ eventcafe.php
@@ -7,10 +7,42 @@
 		echo "Deskripsi Event: ".$row["deskripsievent"]."<br>";
 		echo "Tanggal Event: ".$row["tanggalevent"]."<br>";
 		echo "Kode Promo: ".$row["kodepromoevent"]."<br>";
-		echo "Gambar: <br>";
 		echo "<img src='uploads/".$row['gambarevent']."'  height='150px' width='300px'/>"."<br>";
 		
 		echo "<br />";
+	}
+
+
+	$result = mysqli_query($con, "SELECT * FROM food");
+	while ($row = mysqli_fetch_array($result))
+	{
+			echo "Nama Makanan: ".$row["nama_makanan"]."<br>";
+			echo "Harga: ".$row["harga_makanan"]."<br>";
+			//deskripsi gabisa keluar
+			echo "Deskripsi: ".$row["deskripsi_makanan"]."<br>";
+            
+			//image gabisa keluar
+			echo "<img src='uploads/".$row['image_makanan']."'  height='150px' width='300px'/>"."<br>";
+			
+			echo "<br />";
+			echo "<br />";
+		
+	}
+
+	$result = mysqli_query($con, "SELECT * FROM drink");
+	while ($row = mysqli_fetch_array($result))
+	{
+			echo "Nama Minuman: ".$row["nama_minuman"]."<br>";
+			echo "Harga: ".$row["harga_minuman"]."<br>";
+
+			//deskripsi gabisa keluar
+			echo "Deskripsi: ".$row["deskripsi_minuman"]."<br>";
+
+			//image gabisa keluar
+			echo "<img src='uploads/".$row['image_minuman']."'  height='150px' width='300px'/>"."<br>";
+
+			echo "<br /> <br>";
+		
 	}
 ?>
 
